8f154fa3fb5604a1596a1c698e410e0db1473fa4
dejiowoeye <50265769+Sammorad@users.noreply.github.com>
2026-06-27 19:38:33 +0100

    function to add root created

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 class Node{
     //a Node Class//
-    constructor(Node, left, right){
-      this.Node = Node;
+    constructor(node, left, right){
+      this.node = node;
       this.right = right;
       this.left = left;
     }
@@ -26,6 +26,19 @@ class Tree{
         
     
 }
-    
+ buildTree(array)   {
+    array = this.checkArray(array)
+    if (array.length === 0) return null
+    const mid = Math.floor(array.length/2)
+    const root = new Node(array[mid])
+
+    const leftArray = array.slice(0, mid);
+    const rightArray = array.slice(mid+1)
+    root.left = this.buildTree(leftArray)
+    root.right = this.buildTree(rightArray)
+
+    return root;
+
+ }
 
 }
