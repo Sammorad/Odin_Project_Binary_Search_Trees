8f095126e855914bcced9e79d8505e63291b0d80
dejiowoeye <50265769+Sammorad@users.noreply.github.com>
2026-06-27 14:13:21 +0100

    class of tree and Node inputed

---
+++ index.js
@@ -0,0 +1,14 @@
+class Node{
+    //a Node Class//
+    constructor(Node, left, right){
+      this.Node = Node;
+      this.right = right;
+      this.left = left;
+    }
+}
+class Tree{
+    constructor(){
+        this.root = null;
+    }
+    
+}
