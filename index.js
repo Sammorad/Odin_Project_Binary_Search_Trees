3370aa3aa075ce13a186f4bbf3846ecbb59990f8
dejiowoeye <50265769+Sammorad@users.noreply.github.com>
2026-06-29 11:00:11 +0100

    worked on buildtree includes, search and delete in progress

--- index.js
+++ index.js
@@ -26,19 +26,91 @@ class Tree{
         
     
 }
- buildTree(array)   {
-    array = this.checkArray(array)
-    if (array.length === 0) return null
-    const mid = Math.floor(array.length/2)
-    const root = new Node(array[mid])
+    buildTree(array)   {
+        array = this.checkArray(array)
+        if (array.length === 0) return null
+        const mid = Math.floor(array.length/2)
+        const root = new Node(array[mid])
 
-    const leftArray = array.slice(0, mid);
-    const rightArray = array.slice(mid+1)
-    root.left = this.buildTree(leftArray)
-    root.right = this.buildTree(rightArray)
+        const leftArray = array.slice(0, mid);
+        const rightArray = array.slice(mid+1)
+        root.left = this.buildTree(leftArray)
+        root.right = this.buildTree(rightArray)
 
-    return root;
+        return root;
 
- }
-
+    }
+    checkValue(value){
+        //check the type of value to ensue its an integer 
+        if (!Number.isInteger(value)) return false
+        return true 
 }
+  includes(value){
+    if(!this.checkValue(value))return false;
+    return this._searchNode(this.root, value);
+  }
+
+  _searchNode(node, value){
+    if (node === null) return false;
+    if (node.node === value) return true
+    if (node.node < value ){
+        return this._searchNode(node.right, value)
+    }  else{
+        return this._searchNode(node.left, value)
+    }
+  }
+
+  insert(value){
+    // a function to insert a new value into the tree//
+    if (!this.checkValue(value)){
+        return false 
+    }
+    else if(this.root === null) {
+       this.root = new Node (value)
+       return true
+        }
+
+        return this._insertNode(this.root, value)
+        
+    }
+
+    _insertNode(node, value){
+        if (value < node.node){
+            if (node.left === null){
+                node.left = new Node (value);
+                return true
+            } else {
+                return this._insertNode(node.left, value)
+            }
+        } else {
+            if (node.right === null){
+                node.right = new Node(value);
+                return true;
+            } else{
+                return this._insertNode(node.right, value)
+            }
+        }
+    }
+
+    deleteItem(value){
+        //delete a value from the tree 
+        if (!this.includes(value)) return false
+        if (this.root === value ){
+            if((this.root.right === null )&& (this.root.right === null)){
+                this.root = null
+                return true 
+
+            }
+            else if (this.root.right === null && this.root.left !== null){
+                this.root = this.root.left
+                this.root.left = null
+            }
+            else if (this.root.left === null && this.root.right !== null){
+                this.root = this.root.right;
+                this.root.right = null;
+            }
+
+    }
+    else(){}
+  
+}}
